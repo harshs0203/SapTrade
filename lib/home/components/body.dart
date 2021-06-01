43a8e57579fc9286fe4eea002b35473ea5307f10
Harsh Saini <harshs0203@gmail.com>
2021-06-01 18:27:01 +0530

    Recommended card

--- lib/home/components/body.dart
+++ lib/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sap_trade/home/components/header_with_image.dart';
+import 'package:sap_trade/home/components/titleWithButton.dart';
 
 import '../../constants.dart';
 
@@ -14,30 +15,86 @@ class Body extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           HeaderWithImage(size: size),
-          Container(
-            height: 24,
-            child: Stack(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-                  child: Text(
-                    'Recommended',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  child: Container(
-                    height: 7,
-                    color: kPrimaryColor.withOpacity(0.2),
-                  ),
-                  left: 0.0,
-                  right: 0.0,
-                  bottom: 0.0,
+          TitleWithMoreBtn(title: "Recommended", press: () {}),
+          RecommendedCard(
+            image: 'assets/images/image_1.png',
+            title: 'customer 1',
+            location: 'location',
+            price: 100,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendedCard extends StatelessWidget {
+
+
+  final String image, title, location;
+  final int price;
+  final Function press;
+
+  const RecommendedCard({Key key, this.image, this.title, this.location, this.price, this.press}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: [
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 50,
+                  color: kPrimaryColor.withOpacity(0.3),
                 ),
               ],
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              )
+              ),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: title.toUpperCase()+'\n',
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        TextSpan(
+                          text: location.toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withOpacity(0.5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\₹$price',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: kPrimaryColor),
+                  )
+                ],
+              ),
             ),
           ),
         ],
